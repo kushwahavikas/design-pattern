a82fd6ffa59f7526a1d665e41192480a7bfc4e43
vikas <kushwahavikas766@gmail.com>
2023-03-11 20:09:15 +0530

    message for learning

--- main.cpp
+++ main.cpp
@@ -27,7 +27,22 @@ template <typename T> struct Specification
 };
 template <typename T> struct Filter
 {
-    virtual vector<T*> ProductFilter(vector<T*> items, const Specification<T> &spec) = 0;
+    virtual vector<T*> ProductFilter(vector<T*> items, const Specification<T>& spec) = 0;
+    // The reason & is used because specification struct has become abstract class with virtual function becuase of that we can't 
+    // instantiate an object of it.  And also by using & it now has become an lvalue reference we can only reference to created 
+    // already object.
+    // if we want to send ColorSpecification {Color::green} then we have to make lvalue refrence to be constant and the function
+    //which is using the lvalue reference needs to be constant.
+    // before 
+    //bool is_satisfied(T*) override , 
+    //vector<Product *> ProductFilter(vector<Product *> items, Specification<Product> &spec) override
+    //vector<Product*> result = bf.ProductFilter(items, color);
+    //after 
+    //bool is_satisfied(T*)const override , 
+    //vector<Product *> ProductFilter(vector<Product *> items, const Specification<Product> &spec) override
+    //vector<Product*> result = bf.ProductFilter(items, ColorSpecification {Color::green};
+    // all of this change is because of this  -- ColorSpecification {Color::green} -- which is an rvalue.
+
 };
 
 struct ColorSpecification: Specification<Product>
